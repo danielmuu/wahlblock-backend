e97dcdc97fe2e046a132f8d2c39c82187a6d6d67
danielmuu <danielmuu@users.noreply.github.com>
2018-03-10 01:29:01 +0100

    spaces! and comment

--- src/main/java/de/devdanmu/wahlblockbackend/WahlblockBackendApplication.java
+++ src/main/java/de/devdanmu/wahlblockbackend/WahlblockBackendApplication.java
@@ -3,10 +3,16 @@ package de.devdanmu.wahlblockbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author danmu
+ * created on 2018-03-10
+ */
+
+
 @SpringBootApplication
 public class WahlblockBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WahlblockBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WahlblockBackendApplication.class, args);
+    }
 }
