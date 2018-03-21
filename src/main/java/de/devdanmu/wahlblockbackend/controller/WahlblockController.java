7e4b36345a68d241d6fe7d7892feffc996db34a8
danielmuu <10696844+danielmuu@users.noreply.github.com>
2018-03-21 12:56:58 +0100

    new endpoint hash validation

--- src/main/java/de/devdanmu/wahlblockbackend/controller/WahlblockController.java
+++ src/main/java/de/devdanmu/wahlblockbackend/controller/WahlblockController.java
@@ -1,16 +1,21 @@
 package de.devdanmu.wahlblockbackend.controller;
 
 import de.devdanmu.wahlblockbackend.jpa.Election;
+import de.devdanmu.wahlblockbackend.jpa.HashWrapper;
 import de.devdanmu.wahlblockbackend.jpa.LoginVoter;
 import de.devdanmu.wahlblockbackend.jpa.VoterHash;
 import de.devdanmu.wahlblockbackend.service.ElectionInfoService;
+import de.devdanmu.wahlblockbackend.service.HashValidationService;
 import de.devdanmu.wahlblockbackend.service.VoterLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author danmu
@@ -21,11 +26,13 @@ public class WahlblockController {
 
     private final VoterLoginService voterLoginService;
     private final ElectionInfoService electionInfoService;
+    private final HashValidationService hashValidationService;
 
     @Autowired
-    public WahlblockController(final VoterLoginService voterLoginService, final ElectionInfoService electionInfoService) {
+    public WahlblockController(final VoterLoginService voterLoginService, final ElectionInfoService electionInfoService, final HashValidationService hashValidationService) {
         this.voterLoginService = voterLoginService;
         this.electionInfoService = electionInfoService;
+        this.hashValidationService = hashValidationService;
     }
 
     @PostMapping("/api/v1/voter/login")
@@ -33,9 +40,13 @@ public class WahlblockController {
         return voterLoginService.startVoterLogin(loginVoter);
     }
 
-    @GetMapping("/api/v1/election")
-    public Election getElectionInfo() throws Exception {
-        return electionInfoService.getElection();
+    @GetMapping("/api/v1/election/{electionId}")
+    public Election getElectionInfo(@PathVariable int electionId) throws Exception {
+        return electionInfoService.getElection(electionId);
+    }
 
+    @PostMapping("/api/v1/validation")
+    public List<String> getHashValidation(@RequestBody HashWrapper hashWrapper) {
+        return hashValidationService.validateHashList(hashWrapper.getHashList());
     }
 }
---
+++ src/main/java/de/devdanmu/wahlblockbackend/service/HashValidationService.java
@@ -0,0 +1,47 @@
+package de.devdanmu.wahlblockbackend.service;
+
+import de.devdanmu.wahlblockbackend.jpa.VoterHash;
+import de.devdanmu.wahlblockbackend.repository.HashValidationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author danmu
+ * created on 2018-03-20
+ */
+@Service
+public class HashValidationService {
+
+    private final HashValidationRepository hashValidationRepository;
+
+    @Autowired
+    public HashValidationService(final HashValidationRepository hashValidationRepository) {
+        this.hashValidationRepository = hashValidationRepository;
+    }
+
+    public List<String> validateHashList(final List<String> hashListToValidate) {
+        List<VoterHash> voterHashListToValidate = getListWithVoterHash(hashListToValidate);
+        List<VoterHash> foundValidHashes = hashValidationRepository.findByHashIn(hashListToValidate);
+        voterHashListToValidate.removeAll(foundValidHashes); // removes all valid hash values
+        return getListWithStrings(voterHashListToValidate);
+    }
+
+    private List<VoterHash> getListWithVoterHash(List<String> hashListToValidate) {
+        List<VoterHash> voterHashListToValidate = new ArrayList<>();
+        for(String hash : hashListToValidate) {
+            voterHashListToValidate.add(new VoterHash(hash));
+        }
+        return voterHashListToValidate;
+    }
+
+    private List<String> getListWithStrings(List<VoterHash> voterHashListToValidate) {
+        List<String> hashListWithNonValidHashes = new ArrayList<>();
+        for(VoterHash voterHash : voterHashListToValidate) {
+            hashListWithNonValidHashes.add(voterHash.getHash());
+        }
+        return hashListWithNonValidHashes;
+    }
+}
