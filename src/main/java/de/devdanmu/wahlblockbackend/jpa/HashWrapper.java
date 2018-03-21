26c39e8228a73e01d5639c16278a7ac38df1e4ad
danielmuu <10696844+danielmuu@users.noreply.github.com>
2018-03-21 12:59:13 +0100

    dao for hash

---
+++ src/main/java/de/devdanmu/wahlblockbackend/jpa/HashWrapper.java
@@ -0,0 +1,17 @@
+package de.devdanmu.wahlblockbackend.jpa;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author danmu
+ * created on 2018-03-20
+ */
+@Data
+public class HashWrapper {
+
+    @JsonProperty("data")
+    private List<String> hashList;
+}
---
+++ src/main/java/de/devdanmu/wahlblockbackend/repository/HashValidationRepository.java
@@ -0,0 +1,17 @@
+package de.devdanmu.wahlblockbackend.repository;
+
+import de.devdanmu.wahlblockbackend.jpa.VoterHash;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author danmu
+ * created on 2018-03-20
+ */
+@Repository
+public interface HashValidationRepository extends JpaRepository<VoterHash, String> {
+
+    List<VoterHash> findByHashIn(List<String> hashList);
+}
